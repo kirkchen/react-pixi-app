15718d484f12943cabefc028e66a152acb4c0671
Kirk Chen <rwk0119@yahoo.com.tw>
2018-04-04 22:11:22 +0800

    Add circle with radius between 20 and 50 when click on canvas

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Application } from 'pixi.js';
+import { Application, Graphics } from 'pixi.js';
+
+import { getRandomInt } from '../utils/number';
 
 export default class App extends React.Component {
   constructor() {
     super();
+
+    this.handleCanvasClick = this.handleCanvasClick.bind(this);
   }
 
   componentDidMount() {
@@ -13,13 +17,25 @@ export default class App extends React.Component {
     this.app.start();
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.app.stop();
+  }
+
+  handleCanvasClick(e) {
+    let radius = getRandomInt(20, 50);
+
+    let circle = new Graphics();
+    circle.beginFill(0x9966FF);
+    circle.drawCircle(e.clientX, e.clientY, radius);
+    circle.endFill();
+
+    this.app.stage.addChild(circle);
   }
 
   render() {
     return (
-      <div ref={(element) => this.canvas = element}></div>
+      <div ref={(element) => this.canvas = element}
+        onClick={this.handleCanvasClick}></div>
     );
   }
 }
