d2b0bf8ea61922aa4aaf743dbd4cee956aae692d
Kirk Chen <rwk0119@yahoo.com.tw>
2018-04-05 14:32:20 +0800

    Remove comments

--- src/utils/draggable.js
+++ src/utils/draggable.js
@@ -17,8 +17,6 @@ export function makeSpriteDraggable(target, app, radius) {
     if (drag) {
       drag.position.x = e.data.originalEvent.pageX - app.view.offsetLeft - radius;
       drag.position.y = e.data.originalEvent.pageY - app.view.offsetTop - radius;
-      // drag.position.x += e.data.originalEvent.movementX;
-      // drag.position.y += e.data.originalEvent.movementY;
     }
   })
 }
